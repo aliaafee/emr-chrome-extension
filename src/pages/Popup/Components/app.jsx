4ebc2d630c48856c56ab1915177a2fe8c0c7f420
Dr. Ali Aafee <45202@igmh.gov.mv>
2024-04-23 11:51:16 +0500

    Swtchable views

--- src/pages/Popup/Components/app.jsx
+++ src/pages/Popup/Components/app.jsx
@@ -7,7 +7,12 @@ import LabResultBrowser from "./labresult-browser";
 import ErrorMessage from "./error-message";
 import LoadingSpinner from "./loading-spinner";
 import RadiologyBrowser from "./radiology-browser";
-import { CopyPlusIcon, ExternalLink } from "lucide-react";
+import {
+    CopyPlusIcon,
+    ExternalLink,
+    FlaskConicalIcon,
+    SkullIcon,
+} from "lucide-react";
 import { ToolBar, ToolBarButton } from "./toolbar";
 
 function getUrlParams() {
@@ -33,9 +38,22 @@ export default function App() {
     const [patientId, setPatientId] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [activeView, setActiveView] = useState("radiology");
     const isPopUpWindw = useMemo(() => !getUrlParams().has("tabid"));
 
     useEffect(() => {
+        chrome.storage.sync.get("activeView", function (data) {
+            if (!data.activeView) {
+                handleSetActiveView("radiology");
+                return;
+            }
+            setActiveView(data.activeView);
+        });
+        // chrome.storage.local.get(["activeView"]).then((result) => {
+        //     console.log(result);
+        //     if (result) setActiveView(result);
+        //     handleSetActiveView("radiology");
+        // });
         (async () => {
             setLoading(true);
             try {
@@ -68,6 +86,11 @@ export default function App() {
         }
     };
 
+    const handleSetActiveView = (name) => {
+        setActiveView(name);
+        chrome.storage.sync.set({ activeView: name });
+    };
+
     if (loading) {
         return (
             <div className="w-full h-full flex">
@@ -86,8 +109,22 @@ export default function App() {
 
     return (
         <div className="w-full h-full flex flex-col">
-            <div className="flex items-center border-b-2 border-gray-300">
-                <div className="grow p-1.5">PatientId: {patientId}</div>
+            <div className="flex items-center justify-between border-b-2 border-gray-300">
+                <ToolBar className="">
+                    <ToolBarButton
+                        title="Radiology Browser"
+                        onClick={() => handleSetActiveView("radiology")}
+                    >
+                        <SkullIcon width={16} height={16} />
+                    </ToolBarButton>
+                    <ToolBarButton
+                        title="Lab Result Browser"
+                        onClick={() => handleSetActiveView("lab")}
+                    >
+                        <FlaskConicalIcon width={16} height={16} />
+                    </ToolBarButton>
+                </ToolBar>
+                <div className=" p-1.5">PatientId: {patientId}</div>
                 <ToolBar>
                     {isPopUpWindw ? (
                         <ToolBarButton title="Popout" onClick={handleNewWindow}>
@@ -104,8 +141,17 @@ export default function App() {
                 </ToolBar>
             </div>
 
-            {/* <LabResultBrowser patientId={patientId} targetTabId={targetTabId} /> */}
-            <RadiologyBrowser patientId={patientId} targetTabId={targetTabId} />
+            {activeView == "lab" ? (
+                <LabResultBrowser
+                    patientId={patientId}
+                    targetTabId={targetTabId}
+                />
+            ) : (
+                <RadiologyBrowser
+                    patientId={patientId}
+                    targetTabId={targetTabId}
+                />
+            )}
         </div>
     );
 }
